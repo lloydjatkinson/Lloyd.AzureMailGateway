ad85eca13788d2e3bb4081fa07baf0f0108aea06
Lloyd Atkinson <lloydjatkinson@gmail.com>
2017-10-24 22:22:21 +0100

    Add more documentation

--- src/Lloyd.AzureMailGateway.Models/Bcc.cs
+++ src/Lloyd.AzureMailGateway.Models/Bcc.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 
 namespace Lloyd.AzureMailGateway.Core
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Bcc
     {
+        /// <summary>
+        /// Gets the addresses.
+        /// </summary>
+        /// <value>
+        /// The addresses.
+        /// </value>
         public IEnumerable<Address> Addresses { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bcc"/> class.
+        /// </summary>
+        /// <param name="addresses">The addresses.</param>
+        /// <exception cref="ArgumentNullException">addresses</exception>
         public Bcc(IEnumerable<Address> addresses) => Addresses = addresses ?? throw new ArgumentNullException(nameof(addresses));
     }
 }
